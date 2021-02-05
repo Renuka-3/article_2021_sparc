71cf02308abcf9e61387c9c9f811806be929edae
Renuka-3 <renukapotbhare333@gmail.com>
2021-02-05 17:01:13 +0530

    r

--- create_phyloseq.R
+++ create_phyloseq.R
@@ -29,3 +29,23 @@ sample_data(phy)$age_group <- cut(meta(phy)$Age, breaks = c(0, 40, 59, Inf), lab
 
 saveRDS(phy, file = "data/processed/phyloseq/phy20.1.RDS")
 
+######
+otu_mat    <- as.data.frame(read_excel("data/with_control/otu with control.xlsx"))
+tax_mat    <- as.data.frame(read_excel("data/with_control/taxa with control.xlsx"))
+samples_df <- as.data.frame(read_excel("data/with_control/meta with control.xlsx"))
+
+row.names(otu_mat) <- otu_mat$OTUU
+otu_mat <- otu_mat %>% select (-OTUU)
+row.names(tax_mat) <- tax_mat$OTUU
+tax_mat <- tax_mat %>% select (-OTUU) 
+
+row.names(samples_df) <- samples_df$Sample
+#samples_df <- samples_df %>% select (-Sample) 
+otu_mat <- as.matrix(otu_mat)
+tax_mat <- as.matrix(tax_mat)
+OTU = otu_table(otu_mat, taxa_are_rows = TRUE)
+TAX = tax_table(tax_mat)
+sam = sample_data(as.data.frame(samples_df))
+
+# Add age group information also
+phy <- phyloseq(OTU, TAX, sam)
